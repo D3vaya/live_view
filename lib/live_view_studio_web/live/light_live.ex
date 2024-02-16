bde2bbc598d326082c73cde52180d66f5a2207fe
D3vaya <j.ayala.higuera@gmaial.com>
2024-02-16 16:56:01 -0300

    exercism

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -2,22 +2,32 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    IO.inspect(self(), label: "MOUNT")
-    socket = assign(socket, brightness: 10)
+    socket = assign(socket, brightness: 10, temp: "3000")
     {:ok, socket}
   end
 
   def render(assigns) do
-    IO.inspect(self(), label: "RENDER")
+    # IO.inspect(self(), label: "RENDER")
 
     ~H"""
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%"}>
+        <span style={"width: #{@brightness}%; background: #{temp_color(@temp)}"}>
           <%= @brightness %>%
         </span>
       </div>
+      <form phx-change="update">
+        <input
+          type="range"
+          min="0"
+          max="100"
+          name="brightness"
+          value={@brightness}
+        />
+      </form>
+      <br />
+      <br />
       <button phx-click="off">
         <img src="/images/light-off.svg" />
       </button>
@@ -33,8 +43,40 @@ defmodule LiveViewStudioWeb.LightLive do
       <button phx-click="on">
         <img src="/images/light-on.svg" />
       </button>
+      <br />
+      <br />
+      <form phx-change="change-temp">
+        <div class="temps">
+          <%= for temp <- ["3000", "4000", "5000"] do %>
+            <div>
+              <input
+                type="radio"
+                id={temp}
+                name="temp"
+                value={temp}
+                checked={temp == @temp}
+              />
+              <label for={temp}><%= temp %></label>
+            </div>
+          <% end %>
+        </div>
+      </form>
     </div>
     """
+  end
+
+  def handle_event("change-temp", %{"temp" => temp}, socket) do
+    socket = assign(socket, temp: temp)
+    {:noreply, socket}
+  end
+
+  defp temp_color("3000"), do: "#F1C40D"
+  defp temp_color("4000"), do: "#FEFF66"
+  defp temp_color("5000"), do: "#99CCFF"
+
+  def handle_event("update", %{"brightness" => brightness}, socket) do
+    socket = assign(socket, brightness: String.to_integer(brightness))
+    {:noreply, socket}
   end
 
   def handle_event("random", _, socket) do
